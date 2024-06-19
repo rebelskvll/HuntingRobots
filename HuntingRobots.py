9cf43f356c29448241c69f5aa783eef0dcb52d51
Rebelskvll <raulemoncadat@gmail.com>
2024-06-18 21:16:57 -0500

    Varios cambios realizados
    Se hicieron cambios en la forma en como se procesa el archivo robots.txt, pendiente terminar funcionalidades.

--- HuntingRobots.py
+++ HuntingRobots.py
@@ -1,15 +1,12 @@
-import sys
 import argparse
 import requests
-from urllib import request
-import re
+import sys
 
 # Args for the command line help
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--url', help='URL for hunting the robots.txt')
 args = parser.parse_args()
 
-# Check if the url in robots.txt is available or not, and print error code in case of not.
 def checkconnection(url):
     try:
         response = requests.get(url)
@@ -20,34 +17,31 @@ def checkconnection(url):
     except:
         return False
 
-
 def main ():
     print ('[*] Checking if host is online')
     if (checkconnection(args.url)):
         print ('[+] Host is online')
         print ('[*] Checking if robots.txt is present')
-        if (checkconnection(args.url+"/robots.txt")):
-            print ('[+] We found a robot c[○┬●]כ, happy hunting!')
-            #checkrobot()
+        if (checkconnection(f'{args.url}/robots.txt')):
+            print ('[+] We found robots c[○┬●]כ, happy hunting!')
+            checkrobot(f'{args.url}/robots.txt')
         else:
             print ('[!] No robots this time')
     else:
         print ('[!] Host is down or maybe has some trouble')
 
+def checkrobot(robotsurl):
+    response = requests.get(robotsurl)
+    content = response.text
+    
+    for line in enumerate(content.split('\n')):
+        if ("Disallow: " in line[1]):
+            splited = line[1].split(" ")
+            response = requests.get(f'{args.url}{splited[1]}')
+            if response.status_code == 200:
+                print(f'{args.url}{splited[1]}')
 
-def checkrobot():
-    pass
-    #file = request.urlopen(args.url+"/robots.txt")
-    #for line in file:
-    #    line = line.strip()
-    #    line = str(line)
-    #    line = re.sub(r"\s+", "", line)
-    #    regex = re.findall('Dis.*:(/\S*)', str(line))
-    #    finalstring = ''.join(regex)
-    #    finalstring = finalstring.replace(": ", "")
-    #    if len(regex) > 0:
-    #        finalstring = finalstring.replace("'", "")
-    #        checkcode(args.url+finalstring)
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
